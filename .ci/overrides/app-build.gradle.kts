411d3e67f01119b19453d5a7d24705919c67f07a
umbretevsocial-prog <umbretev.social@gmail.com>
2026-09-18 09:49:04 +0800

    Set PIXNET Tracker v1.6.0

--- .ci/overrides/app-build.gradle.kts
+++ .ci/overrides/app-build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.pixnet.tracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.5.1"
+        versionCode = 12
+        versionName = "1.6.0"
     }
 
     signingConfigs {
