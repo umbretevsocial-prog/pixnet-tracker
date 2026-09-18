b7a26a36bd9a964043b56cc218caa831716960e4
umbretevsocial-prog <umbretev.social@gmail.com>
2026-09-18 08:04:56 +0800

    Bump PIXNET Tracker to v1.5.0

--- .ci/overrides/app-build.gradle.kts
+++ .ci/overrides/app-build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.pixnet.tracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.4.0"
+        versionCode = 10
+        versionName = "1.5.0"
     }
 
     signingConfigs {
